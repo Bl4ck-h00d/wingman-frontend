787d52141eb843037c7d0e90f94e4321409b86d9
Bl4ck-h00d <rohan.kumarjha.cd.mec19@itbhu.ac.in>
2022-06-19 20:22:24 +0530

    add post button added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./App.scss";
 import Header from "./components/header";
 import TrendingTags from "./components/trendingTags";
 import PostContainer from "./components/posts";
+import AddPostButton from "./components/addPostButton";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <PostContainer/>
+      <PostContainer />
       <TrendingTags />
+      <AddPostButton />
     </div>
   );
 }
