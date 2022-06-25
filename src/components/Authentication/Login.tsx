d19bf3f00eb93ba2c63ac28141debd8106c11d70
Shashank-Panda <shashankpanda54@gmail.com>
2022-06-26 00:03:59 +0530

    AuthModal done

--- src/components/Authentication/Login.tsx
+++ src/components/Authentication/Login.tsx
@@ -19,46 +19,66 @@ const Login: React.FC = () => {
     console.log("Failed:", errorInfo);
   };
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-      className="login-form"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: "Please input your username!" }]}
+    <section>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        className="login-form"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 8, span: 16 }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit" loading={loading} onClick={handleOk}>
-          Login
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            loading={loading}
+            onClick={handleOk}
+          >
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+
+      <div className="social-divider">
+        <span className="or">or</span>
+      </div>
+      <div className="signup-options">
+        <Button type="primary">
+          <span className="button-text">Continue with Google</span>
+          <span className="button-icon"></span>
         </Button>
-      </Form.Item>
-    </Form>
+        <Button type="primary">
+          <span className="button-text">Continue with Facebook</span>
+        </Button>
+      </div>
+    </section>
   );
 };
 
