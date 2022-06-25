fd851230aa2d5be672a88b2fb9aa75c6cb9c7ef7
Shashank-Panda <shashankpanda54@gmail.com>
2022-06-25 11:30:07 +0530

    AuthModal added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./search/index";
-import { Button } from "antd";
-import  Logo  from "../assets/img/logo.png";
+import Logo from "../assets/img/logo.png";
+import { Tabs, Button, Checkbox, Form, Input, Modal } from "antd";
+import AuthModal from "./Authentication/AuthModal";
 
 const Header = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <>
       <div className="header-container">
         <span className="header-logo">
-          <a href="http://localhost:3000/"><img src={Logo} alt="Wingman chat" width="140px" height="40px" /></a>
+          <a href="http://localhost:3000/">
+            <img src={Logo} alt="Wingman chat" width="140px" height="40px" />
+          </a>
         </span>
-        <Search />
-        <Button className="btn-bg-gradient signup-btn" type="primary">
-          SignUp
+        <Button
+          type="primary"
+          onClick={()=>setIsModalVisible(true)}
+          className="btn-bg-gradient signup-btn"
+        >
+          Login
         </Button>
       </div>
+      <AuthModal visible={isModalVisible} setVisible={setIsModalVisible}/>
     </>
   );
 };
